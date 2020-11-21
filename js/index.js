a989eb5ac21467ff4cca17dedf994d76d5c79d18
ricardo-faria <ricardo.jonas.faria@gmail.com>
2020-11-21 18:42:26 -0300

    adicionado  dependencias pelo npm, estrutura refeita em bootstrap, adcionado contato por whatsapp

--- js/index.js
+++ js/index.js
@@ -4,10 +4,35 @@ $('a[href^="#"]').on('click', function(event) {
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: target.offset().top-60
+            scrollTop: target.offset().top-50
         }, 1000);
     }
 });
 
 //set the company's years of operation
 $('#age').text(new Date().getFullYear() - 2007);
+
+
+//close menu after click
+$(document).on('click', function(event){
+    let $clickedOn = $(event.target), 
+    $collapsableItems = $('.collapse'), 
+    isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+    isLink = ($clickedOn.closest('a').length == 1),
+    isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0); 
+    if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+        $collapsableItems.each(function(){
+            $(this).collapse('hide'); 
+        }
+        );
+    }
+    }
+); 
+
+
+$(document).ready(function() {
+    setInterval(function() {
+      $(".whatsapp").toggleClass("shk");
+      setTimeout('$(".whatsapp").toggleClass("shk");', 1000);
+    }, 5000);
+  });
